9cbc3665ca0b169d1b6c44cc16222e23f891d05d
batuhancinar <batuhanfaruk1998@gmail.com>
2024-10-13 23:03:03 +0300

    WORK

--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -1,7 +1,55 @@
 package org.example;
 
+import org.junit.platform.commons.util.ToStringBuilder;
+
 public class Player {
     String name;
     int healthPercentage;
     String Weapon ;
+
+    public Player(String name,int healthPercentage,String Weapon){
+        this.name = name;
+        this.healthPercentage = healthPercentage;
+        checkHealt(healthPercentage);
+        this.Weapon= Weapon;
+    }
+    private void checkHealt(int healthPercentage){
+        if(healthPercentage<0){
+            this.healthPercentage=0;
+        }
+        if (healthPercentage>100){
+            this.healthPercentage=100;
+        }
+    }
+
+    public String getWeapon() {
+        return this.Weapon;
+    }
+
+    public int healthRemaining(){
+            return this.healthPercentage;
+        }
+    public void loseHealth(int damage){
+            int healthRemain = healthPercentage - damage;
+            if (healthRemain<0){
+                System.out.println(name + " player has been knocked out of game");
+            }
+            healthPercentage = healthRemain;
+            checkHealt(healthPercentage);
+        }
+    public void restoreHealth(int healthPotion){
+            this.healthPercentage += healthPotion;
+            checkHealt(this.healthPercentage);
+        }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", healthPercentage=" + healthPercentage +
+                ", Weapon='" + Weapon + '\'' +
+                '}';
+    }
+
+
 }
--- src/main/java/org/example/Weapon.java
+++ src/main/java/org/example/Weapon.java
@@ -1,6 +1,23 @@
 package org.example;
 
 public enum Weapon {
-    SWORD
+    SWORD(20,4),
+    SARS(50,60);
 
+    private int damage;
+    private double attackSpeed;
+
+
+     Weapon(int damage, double attackSpeed){
+        this.attackSpeed = attackSpeed;
+        this.damage= damage;
+    }
+
+    public int getDamage() {
+        return this.damage;
+    }
+
+    public double getAttackSpeed() {
+        return this.attackSpeed;
+    }
 }
